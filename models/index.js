55e435127df5690ac8cced6fdcd6dca6ed71eba5
tonisiloniz <tonisilonizbackups@gmail.com>
2023-04-17 22:18:29 +0200

    models/index.js: Definición relaciones entre modelos

--- models/index.js
+++ models/index.js
@@ -3,13 +3,17 @@ const Sequelize = require('sequelize');
 
 // To use SQLite data base:
 // DATABASE_URL = sqlite:db-p5-orm.sqlite
-const url = "sqlite:blog.sqlite";
+const url = process.env.DATABASE_URL || "sqlite:blog.sqlite";
 
 // CREACIÓN DB:
 const sequelize = new Sequelize(url, { logging: false });
 
 // // IMPORTAR DEFINICIONES DE MODELOS:
-// const Author = require('./author')(sequelize, Sequelize.DataTypes);
-// const Post = require('./post')(sequelize, Sequelize.DataTypes);
+const Post = require('./post')(sequelize, Sequelize.DataTypes);
+const Attachment = require('./attachment')(sequelize, Sequelize.DataTypes);
+
+// Relation 1-to-1 between Post and Attachment
+Attachment.hasOne(Post, {as: 'post', foreignKey: 'attachmentId'});
+Post.belongsTo(Attachment, {as: 'attachment', foreignKey: 'attachmentId'});
 
 module.exports = sequelize;
