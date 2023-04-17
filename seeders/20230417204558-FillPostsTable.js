272707a07acee8fddba3a50125e7f0fc8bd29763
tonisiloniz <tonisilonizbackups@gmail.com>
2023-04-17 22:47:14 +0200

    seeders/FillPostsTable.js

--- seeders/20230417204558-FillPostsTable.js
+++ seeders/20230417204558-FillPostsTable.js
@@ -12,6 +12,26 @@ module.exports = {
      *   isBetaMember: false
      * }], {});
     */
+    await queryInterface.bulkInsert('Posts', [
+      {
+          title: 'Primer Post',
+          body: 'Esta práctica implementa un Blog.',
+          createdAt: new Date(),
+          updatedAt: new Date()
+      },
+      {
+          title: 'Segundo Post',
+          body: 'Todo el mundo puede crear posts.',
+          createdAt: new Date(),
+          updatedAt: new Date()
+      },
+      {
+          title: 'Tercer Post',
+          body: 'Cada post puede tener una imagen adjunta.',
+          createdAt: new Date(),
+          updatedAt: new Date()
+      }
+  ]);
   },
 
   async down (queryInterface, Sequelize) {
@@ -21,5 +41,6 @@ module.exports = {
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
+    await queryInterface.bulkDelete('Posts', null, {});
   }
 };
