e489e042d593ff3cb0b6acc4cb6add8a7e4bd6b1
tonisiloniz <tonisilonizbackups@gmail.com>
2023-04-17 22:45:24 +0200

    migrations/CreatePostsTable.js

--- migrations/20230417202000-CreatePostsTable.js
+++ migrations/20230417202000-CreatePostsTable.js
@@ -9,6 +9,46 @@ module.exports = {
      * Example:
      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
      */
+    await queryInterface.createTable(
+      'Posts',
+      {
+          id: {
+              type: Sequelize.INTEGER, //base de datos numero entero
+              allowNull: false,
+              primaryKey: true,
+              autoIncrement: true,
+              unique: true
+          },
+          title: {
+              type: Sequelize.STRING,
+              validate: {notEmpty: {msg: "title must not be empty."}}
+          },
+          body: {
+              type: Sequelize.TEXT,
+              validate: {notEmpty: {msg: "body must not be empty."}}
+          },
+          attachmentId: {
+            type: Sequelize.STRING,
+            references: {
+              model: "Attachments",
+              key: "id"
+            },
+            onUpdate: 'CASCADE',
+            onDelete: 'SET NULL'
+          },
+          createdAt: {
+              type: Sequelize.DATE,
+              allowNull: false
+          },
+          updatedAt: {
+              type: Sequelize.DATE,
+              allowNull: false
+          }
+      },
+      { //si lo modifica algo fuerzo a q se sincronicce
+          sync: {force: true}
+      }
+    );
   },
 
   async down (queryInterface, Sequelize) {
@@ -18,5 +58,6 @@ module.exports = {
      * Example:
      * await queryInterface.dropTable('users');
      */
+    await queryInterface.dropTable('Posts');
   }
 };
